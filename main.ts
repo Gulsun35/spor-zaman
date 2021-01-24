8a5b2510bc3eadfed24d812e9dd206c1703cd492
GulsunTuzun35 <77925175+GulsunTuzun35@users.noreply.github.com>
2021-01-24 17:30:13 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,36 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . # . .
+        # # # # #
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        . # # # .
+        . . # . .
+        . . # . .
+        `)
+})
+basic.forever(function () {
+    basic.showLeds(`
+        # . # . #
+        # # # # #
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        . # # # .
+        . # # # .
+        . . # . .
+        `)
+    basic.pause(100)
+})
